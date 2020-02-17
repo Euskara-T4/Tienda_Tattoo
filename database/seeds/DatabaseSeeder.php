3976d9c478f4fc0492f442d10ff115d45ed9ef60
alex <alex_mendi3@hotmail.com>
2020-02-17 13:50:07 +0100

    fin de seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -9,8 +10,27 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
+    public function run() {
+
+        // Llamada al seed de los USUARIOS
+        self::seedUsuarios();
+        $this->command->info('Tabla USUARIOS inicializada con datos!');
+    }
+
+    // INSERTAMOS EN LA BASE DE DATOS
+    private function seedUsuarios(){
+        // Borramos la tabla
+        DB::table('users')->delete();
+
+        $admin = new User;
+        $admin->izena = "admin";
+        $admin->abizena = "administrador";
+        $admin->email = "admin@gmail.com";
+        $admin->admin = 1;
+        $admin->pasahitza = bcrypt("adminadmin");
+        $admin->save();
+
+        $this->command->info('Usuario nuevo agregado correctamente!');
     }
 }
+
